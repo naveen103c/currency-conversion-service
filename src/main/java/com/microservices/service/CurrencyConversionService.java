6c23b47e48180e1a5ab9e542cd120f5ce702cdcd
naveen103c <naveenchaudhary103@gmail.com>
2023-12-26 13:33:14 +0530

    RestTemplate getForEntity

--- src/main/java/com/microservices/service/CurrencyConversionService.java
+++ src/main/java/com/microservices/service/CurrencyConversionService.java
@@ -4,6 +4,7 @@ import com.microservices.dto.CurrencyConversion;
 import com.microservices.dto.CurrencyExchange;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,15 +17,20 @@ public class CurrencyConversionService {
     @Autowired
     RestTemplate restTemplate;
     public CurrencyConversion generateConversionAmt(String from, String to, int quantity) {
-        CurrencyExchange currencyExchange = restTemplate.getForObject("http://localhost:9081/currency-exchange/from/"+from+"/to/"+to, CurrencyExchange.class);
-        log.info(String.valueOf(currencyExchange));
+//        getForObject
+//        CurrencyExchange currencyExchange = restTemplate.getForObject("http://localhost:9081/currency-exchange/from/"+from+"/to/"+to, CurrencyExchange.class);
+//        getForEntity
+        ResponseEntity<CurrencyExchange> currencyExchangeEntity = restTemplate.getForEntity("http://localhost:9081/currency-exchange/from/" + from + "/to/" + to, CurrencyExchange.class);
         currencyConversion=new CurrencyConversion();
         currencyConversion.setFrom(from);
         currencyConversion.setTo(to);
         currencyConversion.setQuantity(quantity);
-        currencyConversion.setConversionMultiple(currencyExchange.getConversionMultiple());
-        currencyConversion.setTotalCalculatedAmount(currencyExchange.getConversionMultiple()*quantity);
-        currencyConversion.setEnvironment(currencyExchange.getEnvironment());
+        log.info("CurrencyExchange resttemplate call status: "+currencyExchangeEntity.getStatusCode());
+        if(currencyExchangeEntity.getStatusCode().is2xxSuccessful()){
+            currencyConversion.setConversionMultiple(currencyExchangeEntity.getBody().getConversionMultiple());
+            currencyConversion.setTotalCalculatedAmount(currencyExchangeEntity.getBody().getConversionMultiple()*quantity);
+            currencyConversion.setEnvironment(currencyExchangeEntity.getBody().getEnvironment());
+        }
         return currencyConversion;
     }
 }
