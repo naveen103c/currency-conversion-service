513220e19b8cdd020bf9cb2d82076b36264f7e0f
naveen103c <naveenchaudhary103@gmail.com>
2023-12-28 23:21:14 +0530

    Resilience4J circuit breaker

--- src/main/java/com/microservices/resttemplate/controller/CurrencyConversionController.java
+++ src/main/java/com/microservices/resttemplate/controller/CurrencyConversionController.java
@@ -2,9 +2,12 @@ package com.microservices.resttemplate.controller;
 
 import com.microservices.dto.CurrencyConversion;
 import com.microservices.resttemplate.service.CurrencyConversionService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +25,15 @@ public class CurrencyConversionController {
     CurrencyConversionService currencyConversionService;
 
     @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion retrieveConvertedValue(@PathVariable String from, @PathVariable String to, @PathVariable int quantity){
+    @CircuitBreaker(name = "conversionExchangeCircuitBreaker",fallbackMethod = "conversionExchangeFallbackMethod")
+    public ResponseEntity<CurrencyConversion> retrieveConvertedValue(@PathVariable String from, @PathVariable String to, @PathVariable int quantity){
         log.info("retrieve Currency conversion from-"+from+" ,to-"+to);
         CurrencyConversion currencyConversion = currencyConversionService.generateConversionAmt(from,to,quantity);
-        return currencyConversion;
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(currencyConversion);
+    }
+    //creating fallback method
+    public ResponseEntity<CurrencyConversion> conversionExchangeFallbackMethod(String from, String to, int quantity, Exception ex){
+        log.info("called conversionExchangeFallbackMethod method is called. Service Down: " + ex.getMessage());
+        return new ResponseEntity<>(HttpStatusCode.valueOf(503));
     }
 }
